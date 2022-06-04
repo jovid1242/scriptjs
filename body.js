462ac88a884c9ff0cdae3891bac04fe58416139f
jovid1242 <jovid1242@gmail.com>
2022-06-04 18:38:22 +0500

    fixxd

--- body.js
+++ body.js
@@ -1,15 +1,1 @@
 var body = document.getElementsByTagName("body");
-
-function block() {
-  const demoBody = `
-  <center>
-    <h1>
-      Время демо-версии сайта закончилось. Чтобы восстановить работу <br /> сайта обращайтесь к <a href="https://t.me/iamdigitalist">https://t.me/iamdigitalist</a>
-    </h1>
-  </center>
-  `;
-  // body[0].innerHTML = demoBody;
-  // body[0].style.display = "none";
-}
-
-// block();
