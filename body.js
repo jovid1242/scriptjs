2e6defde1b4de915a7952d1ce7745a932dabf812
jovid1242 <jovid1242@gmail.com>
2022-06-05 18:36:54 +0500

    fiz

--- body.js
+++ body.js
@@ -3,8 +3,10 @@ var body = document.getElementsByTagName("body");
 function block() {
   const demoBody = `
   <center>
-    <h1>
-      Время демо-версии сайта закончилось. Чтобы восстановить работу <br /> сайта обращайтесь к <a href="https://t.me/iamdigitalist">https://t.me/iamdigitalist</a>
+    <h1 style="font-size: 24px;
+        font-weight: bold;
+        color: red;">
+      Время демо-версии сайта закончилось 😉. Чтобы восстановить работу <br /> сайта обращайтесь к <a href="https://t.me/iamdigitalist">https://t.me/iamdigitalist</a>
     </h1>
   </center>
   `;
